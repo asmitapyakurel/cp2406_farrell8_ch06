4302d2addea6629a17a0dcc35d746ed7e1dee207
Asmita Pyakurel <asmita.pyakurel@my.jcu.edu.au>
2020-08-11 13:53:59 +1000

    Debug the codes

--- DebuggingExercises/DebugSix1.java
+++ DebuggingExercises/DebugSix1.java
@@ -1,19 +1,25 @@
-// DebugSix1.java
+package DebuggingExercises;// DebugSix1.java
 // Start with a penny
 // double it every day
-   how much do you have in a 30-day month?
+
+import java.util.Scanner;
 public class DebugSix1
 {
-   public static void main(String args[])
+   public static void main(String[] args)
    {
       final int DAYS = 30;
       double money = 0.01;
       int day = 1;
-      while(day < days)
+      Scanner input = new Scanner(System.in);
+      System.out.println("how much do you have in a 30-day month? ");
+
+      while(day < DAYS)
       {
+
          money = 2 * money;
+         day++;
          System.out.println("After day " + day +
-            " you have " + money);
+                 " you have " + money);
       }
    }
 }
